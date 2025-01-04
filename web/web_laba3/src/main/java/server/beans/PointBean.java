c8de943cea5578f9e4027ec9128059828157e018
F4D4 <verdievf@inbox.ru>
2025-01-04 21:58:02 +0300

    Fixing error massage

--- web/web_laba3/src/main/java/server/beans/PointBean.java
+++ web/web_laba3/src/main/java/server/beans/PointBean.java
@@ -49,42 +49,4 @@ public class PointBean {
     public void addPoint() {
         points.add(new Point(Area.Check.calculate(x, y, r), x, y, r));
     }
-
-    public boolean isValid() {
-        if (y == null ||y < -3 || y > 3){
-            return false;
-        }
-
-        double[] validXvalues = { -5 , -4 , -3 , -2 , -1 , 0 , 1 , 2 , 3};
-        boolean xValid = false;
-        for(double validValue : validXvalues){
-            if(x == validValue){
-                xValid = true;
-                break;
-            }
-        }
-
-        if(!xValid||x==null){
-            return false;
-        }
-
-        double[] validRvalues = { -5 , -4 , -3 , -2 , -1 , 0 , 1 , 2 , 3};
-        boolean rValid = false;
-        for(double validValue : validXvalues){
-            if(x == validValue){
-                rValid = true;
-                break;
-            }
-        }
-
-        if(!rValid || r==null){
-            return false;
-        }
-
-        return true;
-
-
-    }
-
-
 }
--- web/web_laba3/src/main/webapp/js/script.js
+++ web/web_laba3/src/main/webapp/js/script.js
@@ -1,23 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById('submitButton');
-    if (button) {
-        button.addEventListener('mouseover', checkButtonState);
+function validateForm(){
+    const xValue = document.getElementById('pointForm:hiddenX').value;
+    const yValue = document.getElementById('pointForm:yValue').value.trim();
+    const rValue = document.getElementById('pointForm:hiddenR').value;
+
+    const numberRegex = /^-?\d+(\.\d{1,15})?$/;
+    if (!numberRegex.test(yValue)) {
+        showError('Значение Y должно быть числом и содержать не более 15 знаков после запятой.');
+        return false;
     }
-});
+
+    if (!numberRegex.test(xValue)) {
+        showError('Пожалуйста , выберете x');
+        return false;
+    }
+
+    if (!numberRegex.test(rValue)) {
+        showError('Пожалуйста , выберете r');
+        return false;
+    }
+
+    const yNum = parseFloat(yValue);
+
+    if(isNaN(yNum) || yNum<-3 || yNum > 3){
+        showError('Введите корректное значение для Y (в диапазоне от -3 до 3).')
+        return false;
+    }
+    return true;
+
+}
 
 
-function checkButtonState() {
-    const button = document.getElementById('submitButton');
-    const errorMessage = document.getElementById('error-message');
+function showError(massage) {
+    const errorDiv = document.getElementById('error-message');
     const errorText = document.getElementById('error-text');
 
-    if (button.disabled) {
-        errorText.textContent = 'Пожалуйста, введите валидные значения';
-        errorMessage.style.display = 'block';
+    // Устанавливаем текст ошибки
+    errorText.textContent = massage ;
 
-        // Скрыть сообщение через 3 секунды
-        setTimeout(() => {
-            errorMessage.style.display = 'none';
-        }, 3000);
-    }
+    // Показываем сообщение об ошибке
+    errorDiv.style.display = 'block';
+
+    // Скрываем сообщение через 3 секунды
+    setTimeout(() => {
+        errorDiv.style.display = 'none';
+    }, 5000); // Через 3 секунды скрываем сообщение
 }
