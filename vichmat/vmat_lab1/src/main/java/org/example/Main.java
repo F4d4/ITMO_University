3aad3e651c131b8030d4b8999c9649e1fce6bfa1
F4D4 <verdievf@inbox.ru>
2025-03-05 14:13:59 +0300

    lab_fully_works

--- vichmat/vmat_lab1/src/main/java/org/example/Main.java
+++ vichmat/vmat_lab1/src/main/java/org/example/Main.java
@@ -1,14 +1,60 @@
 package org.example;
 
 
+import java.io.File;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Выберите как вы хотите вводить данные. 1- клвиатура ; 2 - файл :");
         try{
-            //double[][] arguments = {{2, 2, 10, 14}, {10, 1, 1, 12}, {2, 10, 1, 13}};
-            Scanner scanner = new Scanner(System.in);
+            int userChoice = Integer.valueOf(scanner.nextLine().trim());
+            if(userChoice == 2){
+                try {
+                    System.out.println("Введите название файла:");
+                    String filename = scanner.nextLine().trim();
+                    Scanner scannerFile = new Scanner(new File(filename));
+                    int sizeFromFile = Integer.valueOf(scannerFile.nextLine().trim());
+                    System.out.println(sizeFromFile);
+                    double[][] argumentsFromFile = new double[sizeFromFile][sizeFromFile+1];
+                    for (int i = 0; i < sizeFromFile; i++) {
+                        argumentsFromFile[i] = Arrays.stream(scannerFile.nextLine().trim().split(" "))
+                                .mapToDouble(Double::parseDouble)
+                                .limit(sizeFromFile+1)
+                                .toArray();
+                    }
+                    if (ensureDiagonalDominance(argumentsFromFile, sizeFromFile)) {
+                        System.out.println("Матрица приведена к диагональному преобладанию.");
+                        System.out.println("Обновленная матрица:");
+                        printMatrix(argumentsFromFile);
+                    } else {
+                        System.out.println("Подходящей перестановки нет.");
+                        System.exit(0);
+                    }
+                    double marginFromFile = Double.parseDouble(scannerFile.nextLine().trim());
+                    GaussZeidel gaussZeidel = new GaussZeidel(argumentsFromFile, sizeFromFile,marginFromFile);
+                    gaussZeidel.startAlgorithm();
+
+                    scannerFile.close();
+                    System.exit(0);
+                } catch (Exception e) {
+                    System.err.println("Ошибка чтения файла :  " + e.getMessage());
+                    System.exit(0);
+                }
+            }
+            if(userChoice !=1){
+                System.out.println("Некорректные данные");
+                System.exit(0);
+            }
+        }catch (Exception e ){
+            System.out.println("Некрректные данные");
+            System.exit(0);
+        }
+        try{
             System.out.println("Введите размерность матрицы (n<=20) :");
             int size = Integer.valueOf(scanner.nextLine().trim());
             if(size>20){
@@ -25,12 +71,13 @@ public class Main {
                         .toArray();
             }
             // Вызываем метод для проверки и перестановки
-            if (ensureDiagonalDominance(arguments, 3)) {
+            if (ensureDiagonalDominance(arguments, size)) {
                 System.out.println("Матрица приведена к диагональному преобладанию.");
                 System.out.println("Обновленная матрица:");
                 printMatrix(arguments);
             } else {
                 System.out.println("Подходящей перестановки нет.");
+                System.exit(0);
             }
 
             System.out.println("Введите точность");
