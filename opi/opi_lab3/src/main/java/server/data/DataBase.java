8165587613e6a2472fedc1d53fb4a43effac3ced
User 0 <0@example.com>
2025-05-05 19:47:02 +0300

    Выполнены пункты 4-6 лабораторной работы 3 по ОПИ

--- opi/opi_lab3/src/main/java/server/data/DataBase.java
+++ opi/opi_lab3/src/main/java/server/data/DataBase.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 
 
 public class DataBase {
-    private static final String URL = "jdbc:postgresql://pg:5432/studs";
-    private static final String USER = "s408367";
-    private static final String PASSWORD = "QpjZpMmemnlctw7E";
+    private static final String URL = "jdbc:postgresql://localhost:5432/studs";
+    private static final String USER = "mike-yasnov";
+    private static final String PASSWORD = "20348722";
 
 
 
